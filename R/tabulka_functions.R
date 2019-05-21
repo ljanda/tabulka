01686a731b282f011ee7421ff93a94ab19bd6493
Ludmila Janda <ludmilajanda@Ludmilas-MacBook-Air.local>
2019-05-21 10:03:34 -0400

    add all funcions

---
+++ R/tabulka_functions.R
@@ -0,0 +1,67 @@
+#' @title Categorical Table Function Arranged by Percentage Value
+#'
+#' @description This function allows you to make a kabled table of count and pct
+#' @param df A dataframe
+#' @param var1 A variable
+#' @keywords table, count, percent
+#' @examples
+#' n_pct(df, var1)
+#' @export
+n_pct <- function(df, var1) {
+  var1 <- enquo(var1)
+
+  df %>%
+    group_by(!!var1) %>%
+    summarise(Number = n()) %>%
+    mutate(Percent = round((Number/sum(Number, na.rm = T)*100), 2)) %>%
+    arrange(desc(Percent)) %>%
+    kable(format = "markdown", format.args = list(big.mark = ',')) %>%
+    kable_styling(full_width = F, position = "left")
+}
+
+#' @title Categorical Table Function Arranged by Factor Levels
+#'
+#' @description This function allows you to make a kabled table of count and pct and arrange it by the factor levels
+#' @param df A dataframe
+#' @param var1 A factor variable
+#' @keywords table, count, percent
+#' @examples
+#' n_pct(df, var1)
+#' @export
+n_pct_fct <- function(df, var1) {
+  var1 <- enquo(var1)
+
+  df %>%
+    group_by(!!var1) %>%
+    summarise(Number = n()) %>%
+    mutate(Percent = round((Number/sum(Number, na.rm = T)*100), 2)) %>%
+    arrange(!!var1) %>%
+    kable(format = "markdown", format.args = list(big.mark = ',')) %>%
+    kable_styling(full_width = F, position = "left")
+}
+
+#' @title Continuous Table Function
+#'
+#' @description This function allows you to make a kabled table of Q1, mean, median, Q2, max
+#' @param x A dataframe
+#' @param y A grouping variable
+#' @param z A continuous variable
+#' @keywords table, Q1, mean, median, Q2, max
+#' @examples
+#' n_pct(df, var1, var2)
+#' @export
+shelve <- function(df, var1, var2) {
+  var1 <- enquo(var1)
+  var2 <- enquo(var2)
+
+  df %>%
+    group_by(!!var1) %>%
+    summarise(Q1 = round(quantile(!!var2, 1/4), 2),
+              Median = round(median(!!var2), 2),
+              Mean = round(mean(!!var2), 2),
+              Q3 = round(quantile(!!var2, 3/4), 2),
+              Max = round(max(!!var2), 2)) %>%
+    kable(format = "markdown", format.args = list(big.mark = ','),
+          col.names = c("Group", "Q1", "Median", "Mean", "Q3", "Max")) %>%
+    kable_styling(full_width = F, position = "left")
+}
---
+++ README.md
@@ -0,0 +1,73 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# dinner.table()
+
+The goal of dinner.table() is to make some nice tables.
+
+## Installation
+
+You can install dinner.table from github with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("ljanda/tabulka")
+```
+
+## Examples
+
+### n\_pct()
+
+The n\_pct() function gives a nicely formatted table for a categorical
+variable with the count and percent for each category. The table is
+arranged using the percent in descending order so that the biggest
+categories are at the top of the table.
+
+``` r
+dinner.table::n_pct(diamonds, cut)
+```
+
+| cut       | Number | Percent |
+| :-------- | -----: | ------: |
+| Ideal     | 21,551 |   39.95 |
+| Premium   | 13,791 |   25.57 |
+| Very Good | 12,082 |   22.40 |
+| Good      |  4,906 |    9.10 |
+| Fair      |  1,610 |    2.98 |
+
+### n\_pct\_fct()
+
+The n\_pct\_fct() function gives a nicely formatted table for a factor
+variable with the count and percent for each factor. The table is
+arranged using the factor levels so that the preferred order is
+maintained.
+
+``` r
+dinner.table::n_pct_fct(diamonds, cut)
+```
+
+| cut       | Number | Percent |
+| :-------- | -----: | ------: |
+| Fair      |  1,610 |    2.98 |
+| Good      |  4,906 |    9.10 |
+| Very Good | 12,082 |   22.40 |
+| Premium   | 13,791 |   25.57 |
+| Ideal     | 21,551 |   39.95 |
+
+### shelve()
+
+The shelve() function gives a nicely formatted table giving the Q1,
+Median, Mean, and Q3 for a continous variable that is broken down by
+categories from a categorical variable
+
+``` r
+dinner.table::shelve(diamonds, cut, price)
+```
+
+| Group     |       Q1 |  Median |     Mean |       Q3 |    Max |
+| :-------- | -------: | ------: | -------: | -------: | -----: |
+| Fair      | 2,050.25 | 3,282.0 | 4,358.76 | 5,205.50 | 18,574 |
+| Good      | 1,145.00 | 3,050.5 | 3,928.86 | 5,028.00 | 18,788 |
+| Very Good |   912.00 | 2,648.0 | 3,981.76 | 5,372.75 | 18,818 |
+| Premium   | 1,046.00 | 3,185.0 | 4,584.26 | 6,296.00 | 18,823 |
+| Ideal     |   878.00 | 1,810.0 | 3,457.54 | 4,678.50 | 18,806 |
